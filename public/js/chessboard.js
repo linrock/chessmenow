4cc47009c85b9681b67ee4c35a75d02dc28b760d
Linmiao Xu <linmiao.xu@gmail.com>
2011-01-01 20:34:17 -0800

    Add chessboard.js for managing chessboard pieces.

---
+++ public/js/chessboard.js
@@ -0,0 +1,18 @@
+var initializeBoard = function() {
+  $("#a2 > .piece").addClass('white-pawn');
+  $("#b2 > .piece").addClass('white-pawn');
+  $("#c2 > .piece").addClass('white-pawn');
+  $("#d2 > .piece").addClass('white-pawn');
+  $("#e2 > .piece").addClass('white-pawn');
+  $("#f2 > .piece").addClass('white-pawn');
+  $("#g2 > .piece").addClass('white-pawn');
+  $("#h2 > .piece").addClass('white-pawn');
+  $("#a1 > .piece").addClass('white-rook');
+  $("#h1 > .piece").addClass('white-rook');
+  $("#b1 > .piece").addClass('white-knight');
+  $("#g1 > .piece").addClass('white-knight');
+  $("#c1 > .piece").addClass('white-bishop');
+  $("#f1 > .piece").addClass('white-bishop');
+  $("#d1 > .piece").addClass('white-queen');
+  $("#e1 > .piece").addClass('white-king');
+}
