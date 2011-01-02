09d1e66dccfd9998867da86cf9f9b7bec615e5ef
Linmiao Xu <linmiao.xu@gmail.com>
2011-01-01 21:34:02 -0800

    Add hasPiece method for checking if piece exists at position.

--- public/js/chessboard.js
+++ public/js/chessboard.js
@@ -1,36 +1,36 @@
 var initializeBoard = function() {
-  $("#a2 > .piece").addClass('white-pawn');
-  $("#b2 > .piece").addClass('white-pawn');
-  $("#c2 > .piece").addClass('white-pawn');
-  $("#d2 > .piece").addClass('white-pawn');
-  $("#e2 > .piece").addClass('white-pawn');
-  $("#f2 > .piece").addClass('white-pawn');
-  $("#g2 > .piece").addClass('white-pawn');
-  $("#h2 > .piece").addClass('white-pawn');
-  $("#a1 > .piece").addClass('white-rook');
-  $("#h1 > .piece").addClass('white-rook');
-  $("#b1 > .piece").addClass('white-knight');
-  $("#g1 > .piece").addClass('white-knight');
-  $("#c1 > .piece").addClass('white-bishop');
-  $("#f1 > .piece").addClass('white-bishop');
-  $("#d1 > .piece").addClass('white-queen');
-  $("#e1 > .piece").addClass('white-king');
-  $("#a7 > .piece").addClass('black-pawn');
-  $("#b7 > .piece").addClass('black-pawn');
-  $("#c7 > .piece").addClass('black-pawn');
-  $("#d7 > .piece").addClass('black-pawn');
-  $("#e7 > .piece").addClass('black-pawn');
-  $("#f7 > .piece").addClass('black-pawn');
-  $("#g7 > .piece").addClass('black-pawn');
-  $("#h7 > .piece").addClass('black-pawn');
-  $("#a8 > .piece").addClass('black-rook');
-  $("#h8 > .piece").addClass('black-rook');
-  $("#b8 > .piece").addClass('black-knight');
-  $("#g8 > .piece").addClass('black-knight');
-  $("#c8 > .piece").addClass('black-bishop');
-  $("#f8 > .piece").addClass('black-bishop');
-  $("#d8 > .piece").addClass('black-queen');
-  $("#e8 > .piece").addClass('black-king');
+  $("#a2 > div").addClass('piece white-pawn');
+  $("#b2 > div").addClass('piece white-pawn');
+  $("#c2 > div").addClass('piece white-pawn');
+  $("#d2 > div").addClass('piece white-pawn');
+  $("#e2 > div").addClass('piece white-pawn');
+  $("#f2 > div").addClass('piece white-pawn');
+  $("#g2 > div").addClass('piece white-pawn');
+  $("#h2 > div").addClass('piece white-pawn');
+  $("#a1 > div").addClass('piece white-rook');
+  $("#h1 > div").addClass('piece white-rook');
+  $("#b1 > div").addClass('piece white-knight');
+  $("#g1 > div").addClass('piece white-knight');
+  $("#c1 > div").addClass('piece white-bishop');
+  $("#f1 > div").addClass('piece white-bishop');
+  $("#d1 > div").addClass('piece white-queen');
+  $("#e1 > div").addClass('piece white-king');
+  $("#a7 > div").addClass('piece black-pawn');
+  $("#b7 > div").addClass('piece black-pawn');
+  $("#c7 > div").addClass('piece black-pawn');
+  $("#d7 > div").addClass('piece black-pawn');
+  $("#e7 > div").addClass('piece black-pawn');
+  $("#f7 > div").addClass('piece black-pawn');
+  $("#g7 > div").addClass('piece black-pawn');
+  $("#h7 > div").addClass('piece black-pawn');
+  $("#a8 > div").addClass('piece black-rook');
+  $("#h8 > div").addClass('piece black-rook');
+  $("#b8 > div").addClass('piece black-knight');
+  $("#g8 > div").addClass('piece black-knight');
+  $("#c8 > div").addClass('piece black-bishop');
+  $("#f8 > div").addClass('piece black-bishop');
+  $("#d8 > div").addClass('piece black-queen');
+  $("#e8 > div").addClass('piece black-king');
   chess.load('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
 }
 
@@ -38,4 +38,8 @@ var movePiece = function(from, to) {
   var newClass = $('#' + from + ' > .piece').attr('class').replace('piece ', '');
   $('#' + from + ' > .piece').removeClass(newClass);
   $('#' + to + ' > .piece').addClass(newClass);
+}
+
+var hasPiece = function(position) {
+  return $("#" + position + " > .piece").length !== 0
 }
