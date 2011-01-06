c692a718c7447c21479fc54fbf7b272d984b81a9
Linmiao Xu <linmiao.xu@gmail.com>
2011-01-05 18:43:16 -0800

    Edit compass config to use correct media paths.

--- config/compass.rb
+++ config/compass.rb
@@ -2,6 +2,11 @@ require 'fancy-buttons'
 
 http_path = "/"
 sass_dir = "sass"
+
+http_stylesheets_path = "/media/css"
+http_images_path = "/media/img"
+http_javascripts_path = "/media/js"
+
 css_dir = "public/css"
 images_dir = "public/img"
 javascripts_dir = "public/js"
